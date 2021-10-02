a0450e136e3ed2e9a2204c805ff412afc2a13376
maxim22351 <max.furman2014@gmail.com>
2021-10-02 20:14:59 +0300

    ready auth and exit

--- src/Componets/Main.js
+++ src/Componets/Main.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useContext} from "react";
+import axios from "axios";
 import {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
+import firebase from "firebase/compat";
+
 
 import {urlAllProduct} from "../url.json"
+import {Context} from "../index";
 
 import ProductList from "./ProductList";
 import Basket from "./Basket";
+import Loading from "./Loading";
 
 import {add,
     reset,
@@ -16,12 +21,19 @@ import {add,
     statusModalCheck,
     historySelect
 } from '../store/ProductSlice'
+import {useAuthState} from "react-firebase-hooks/auth";
+
+
 
 
 
 
 export default function Main (){
-    const axios = require('axios');
+
+    const {auth} = useContext(Context);
+    const [user,loading] = useAuthState(auth);
+
+
 
     const refUl = React.createRef();
 
@@ -37,8 +49,6 @@ export default function Main (){
 
     const dispatch = useDispatch();
 
-
-
     // Seact
     const filterCountries = products.filter(item => item.name.toLowerCase().includes(inputSearch.toLowerCase()));
 
@@ -48,8 +58,7 @@ export default function Main (){
     }, {});
 
 
-
-
+    // Get Product
     useEffect(()=>{
         axios.get(urlAllProduct)
             .then(async resp => {
@@ -57,10 +66,7 @@ export default function Main (){
                await setProductReset(resp.data)
                await dispatch(reset(resp.data))
             })
-
     },[])
-
-
 
 
     // Filter
@@ -81,6 +87,7 @@ export default function Main (){
         e.target.classList.add('active')
     }
 
+
     // Basket
     function checkProduct (e){
         let dataset = e.target.dataset;
@@ -89,7 +96,7 @@ export default function Main (){
         if (event.classList.contains('star') || event.classList.contains('btn')){
             e.preventDefault();
 
-            if (authСondition){
+            if (user !== null){
                 if (dataset.key !== undefined) {
                     let a = [];
                     dispatch(add(dataset.key))
@@ -110,55 +117,93 @@ export default function Main (){
     }
 
 
+    // Auth
+    async function clickAuth (){
+        const provider =  new firebase.auth.GoogleAuthProvider()
+        const {user} =  await auth.signInWithPopup(provider)
+
+    }
+
+
     return (
-        <>
+        (loading)?(
+            <Loading/>
+        )
+        :
+        (
+            <>
+                <div className="container">
 
-            <div className="container">
-
-                <Basket/>
-                <form>
-                    <div className="mb-3">
-                        <label  className="form-label" >Поиск:</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="exampleInputEmail1"
-                            aria-describedby="emailHelp"
-                            onInput={(e => setInputSearch(e.target.value))}
-                        />
+                    <Basket/>
+                    <div className="row">
+                        <div className="col-2 offset-10 mt-2 d-flex flex-column">
+                            {
+                                (user !== null)?(
+                                        <>
+                                            <h4 className='auth_block'>{user.displayName}</h4>
+                                            <img src={user.photoURL} alt={user.displayName} className='auth_block'/>
+                                            <button
+                                                className='btn btn-danger mt-2 auth_block'
+                                                style={{'width': '50%'}}
+                                                onClick={()=> auth.signOut()}
+                                            >Выход</button>
+                                        </>
+                                    )
+                                    :
+                                    (
+                                        <button
+                                            className='btn btn-success'
+                                            style={{'float':'right'}}
+                                            onClick={clickAuth}
+                                        >Авторизация</button>
+                                    )
+                            }
+                        </div>
                     </div>
-                </form>
-
-                <div className='mb-2'onClick={filterProduct}>
-                    <ul className="list-group"  ref={refUl}>
-                        <li
-                            className="list-group-item active"
-                            data-price = "popular">Популярные
-                        </li>
-                        <li
-                            className="list-group-item"
-                            data-price = "cheap">От дешевых к дорогим
-                        </li>
-                        <li
-                            className="list-group-item"
-                            data-price = "dear">От дорогих к дешевым
-                        </li>
-                    </ul>
-                </div>
-                <div className="row" onClick={checkProduct}>
-                    {
-                        filterCountries.map(item=>(
-                            <ProductList
-                                key = {item.alpha2Code + item.name}
-                                name = {item.name}
-                                flag = {item.flags.png}
-                                price = {item.population}
-                                keys = {item.area}
+                    <form>
+                        <div className="mb-3">
+                            <label  className="form-label" >Поиск:</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                onInput={(e => setInputSearch(e.target.value))}
                             />
-                        ))
-                    }
+                        </div>
+                    </form>
+
+                    <div className='mb-2'onClick={filterProduct}>
+                        <ul className="list-group"  ref={refUl}>
+                            <li
+                                className="list-group-item active"
+                                data-price = "popular">Популярные
+                            </li>
+                            <li
+                                className="list-group-item"
+                                data-price = "cheap">От дешевых к дорогим
+                            </li>
+                            <li
+                                className="list-group-item"
+                                data-price = "dear">От дорогих к дешевым
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="row" onClick={checkProduct}>
+                        {
+                            filterCountries.map(item=>(
+                                <ProductList
+                                    key = {item.alpha2Code + item.name}
+                                    name = {item.name}
+                                    flag = {item.flags.png}
+                                    price = {item.population}
+                                    keys = {item.area}
+                                />
+                            ))
+                        }
+                    </div>
                 </div>
-            </div>
-        </>
+            </>
+        )
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
 import {Provider} from "react-redux";
 import store from './store'
+
+import App from './App';
+
+import firebase from "firebase/compat";
+import 'firebase/firestore'
+import 'firebase/auth'
+
+
+firebase.initializeApp({
+    apiKey: "AIzaSyAzbNkLYbVtkGKN4RtJ8_MFFVfWFpogf50",
+    authDomain: "shop-react-e1241.firebaseapp.com",
+    projectId: "shop-react-e1241",
+    storageBucket: "shop-react-e1241.appspot.com",
+    messagingSenderId: "387515580583",
+    appId: "1:387515580583:web:c338c262e63d45e4bfdb82",
+    measurementId: "G-X0DBRH2KKP"
+})
+
+export const Context = createContext(null)
+
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
 
 ReactDOM.render(
   <React.StrictMode>
       <Provider store={store}>
-          <App />
+          <Context.Provider value={{
+              firebase,
+              auth,
+              firestore
+          }}>
+              <App />
+          </Context.Provider>
       </Provider>
   </React.StrictMode>,
   document.getElementById('root')
